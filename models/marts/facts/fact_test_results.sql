ed8b25b0d67d2ebdabbafac010103cf6c0524470
CADUPEE <cadupee@icloud.com>
2026-09-08 17:55:48 +0000

    Corrected fact table references

--- models/marts/facts/fact_test_results.sql
+++ models/marts/facts/fact_test_results.sql
@@ -1,11 +1,16 @@
 SELECT
-sc.School_ID,
-st.StudentIdentifier,
-st.SchoolYear,
-st.AssessmentName,
-st.GradeLevelWhenAssessed,
-st.ScaleScoreAchievementLevel,
-st.ScaleScore
-FROM {{ ref('stg_state_testing')  }} as st
+sc.SCHOOL_ID,
+st.STUDENTIDENTIFIER,
+asm.ASSESSMENTNAME,
+stg.SUBMITDATETIME,
+stg.SCHOOLYEAR,
+stg.GRADELEVELWHENASSESSED,
+stg.SCALESCOREACHIEVEMENTLEVEL,
+stg.SCALESCORE
+FROM {{ ref('stg_state_testing')  }} as stg
 LEFT JOIN {{  ref('dim_schools')  }} as sc
-ON st.SchoolName = sc.School_Name
+ON stg.SchoolName = sc.School_Name
+LEFT JOIN {{  ref('dim_students')  }} as st
+ON stg.STUDENTIDENTIFIER = st.STUDENTIDENTIFIER
+LEFT JOIN {{  ref('dim_assessments')  }} as asm
+ON stg.ASSESSMENTNAME = asm.ASSESSMENTNAME
